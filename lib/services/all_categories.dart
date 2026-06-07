e57a6b5dd5de2395aaffd291844ac61450875560
moalaa125 <muhamedawadkassem@gmail.com>
2026-06-07 18:36:35 +0300

    finish git requests

---
+++ lib/services/all_categories.dart
@@ -0,0 +1,15 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+class GetAllCategories {
+  Future<List<dynamic>> getAllCategories() async {
+    http.Response response = await http.get(
+      Uri.parse('https://fakestoreapi.com/products/categories'),
+    );
+    List<dynamic> data = jsonDecode(response.body);
+    print(data);
+    return data;
+    
+  }
+}
---
+++ lib/services/categories_service.dart
@@ -0,0 +1,22 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:store_app/models/product_model.dart';
+
+class CategoriesService {
+  Future<List<ProductModel>> getcategories() async{
+      http.Response response = await http.get(
+      Uri.parse('https://fakestoreapi.com/products'),
+    );
+    List<dynamic> data = jsonDecode(response.body);
+
+    List<ProductModel> ProductList = [];
+
+    for (int i = 0; i < data.length; i++) {
+      ProductList.add(ProductModel.fromJson(data[i]));
+      print(ProductList[i].id);
+    }
+    return ProductList;
+
+  }
+}
